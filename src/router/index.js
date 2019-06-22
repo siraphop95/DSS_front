df01fe42a85366cdff62057326309040d1c66195
siraphop95 <siraphop.amo@student.mahidol.edu>
2019-06-22 16:15:01 +0700

    test4

--- src/router/index.js
+++ src/router/index.js
@@ -15,8 +15,8 @@ import Test from '@/components/Test'
 import Louise from '@/components/Louise'
 import LouiseTest from '@/components/LouiseTest'
 import Navbar from '@/components/Navbar'
-import DocumentDetail from '@/components/documentDetail'
-import SuggestedDocuments from '@/components/Home'
+import DocumentDetail from '@/components/DocumentDetail'
+import SuggestedDocuments from '@/components/SuggestedDocuments'
 import Answer from '@/components/Answer'
 Vue.use(Router)
 
